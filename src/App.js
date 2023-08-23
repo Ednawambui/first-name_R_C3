353684507c2cbad99cc80f667380a793cf55d0a1
Edna <Ednawambui254@gmail.com>
2023-08-23 12:15:43 +0300

    Introduction to React

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <p>I am Edna Wambui Kamiti this is my first React Application.</p>
+    </div>
+  );
+}
+
+export default App;
