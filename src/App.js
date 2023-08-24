e06bc7f3f001c62a66efda7b977a012243e0e200
Edna <Ednawambui254@gmail.com>
2023-08-25 00:40:18 +0300

    Assignment 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function Portfolio() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-      <p>I am Edna Wambui Kamiti this is my first React Application.</p>
+    <div className="portfolio">
+      <h1>Edna Wambui Kamiti</h1>
+      <section className="education">
+        <h2>Education</h2>
+        <p>Diploma in Business Information Technology</p>
+        <p>Kca University</p>
+      </section>
+      <section className="professional">
+        <h2>Professional Background</h2>
+        <p>Software Developer at Pertom Digital</p>
+      </section>
+      <section className="skills">
+        <h2>Skills</h2>
+        <ul>
+          <li>Html</li>
+          <li>Css</li>
+          <li>JavaScript</li>
+          <li>React</li>
+          <li>Git</li>
+          </ul>
+      </section>
     </div>
   );
 }
 
-export default App;
+export default Portfolio;
